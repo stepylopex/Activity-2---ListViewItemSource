ac820654133951a2070ed1142df824803d156cd5
stepylopex <83437121+stepylopex@users.noreply.github.com>
2022-05-30 12:35:22 +0800

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MOD04_PDC03
+{
+   public class Employee
+    {
+        public string DisplayName { get; set; }
+        public string Position { get; set; }
+        public string Image { get; set; }
+    }
+}
+
